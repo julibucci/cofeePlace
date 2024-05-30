bb23477735ee9dc8416f3f5559ace23d37e1ef04
MLuciaPerez <luu0808perez@gmail.com>
2024-05-29 21:55:32 -0300

    Clase receta completa

---
+++ src/modelo/Receta.java
@@ -0,0 +1,30 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receta {
+    private String nombrePlato;
+    private ArrayList<Ingrediente> ingredientes;
+
+    public Receta(String nombrePlato, ArrayList<Ingrediente> ingredientes) {
+        this.nombrePlato = nombrePlato;
+        this.ingredientes = ingredientes;
+    }
+
+    public String getNombrePlato() {
+        return nombrePlato;
+    }
+
+    public ArrayList<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Receta{" +
+                "nombrePlato='" + nombrePlato + '\'' +
+                ", ingredientes=" + ingredientes +
+                '}';
+    }
+}
